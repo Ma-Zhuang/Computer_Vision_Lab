f6061f68db3a0c081849af427d02be921c14ae4d
Ma-Zhuang <2686224016@qq.com>
2021-05-11 19:30:39 +0100

    Week5

--- Week5/SIFT/main.m
+++ Week5/SIFT/main.m
@@ -1,9 +1,14 @@
+% img_1 = imread('Test_img\print.PNG');
+% img_1_gary = rgb2gray(img_1);
+% [height,width] = size(img_1_gary);
+% output = zeros(round(height/2),round(width/2));
+% for i =1:2:height
+%     for j = 1:2:width
+%         output(i,j) = img_1_gary(i,j);
+%     end
+% end
 img_1 = imread('Test_img\print.PNG');
-img_1_gary = rgb2gray(img_1);
-[height,width] = size(img_1_gary);
-output = zeros(round(height/2),round(width/2));
-for i =1:2:height
-    for j = 1:2:width
-        output(i,j) = img_1_gary(i,j);
-    end
-end
+sigma = 500;
+noise = randn(size(img_1)).*sigma;
+output = double(img_1)+noise;
+imshow(output);
